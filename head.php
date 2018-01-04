4580391db87eef37bbe7f2cdf98b9352a659fcd2
nabby27 <ivancordoba77@gmail.com>
2018-01-04 16:43:37 +0100

    Solved title bug

--- head.php
+++ head.php
@@ -8,28 +8,17 @@ elseif (empty($_SESSION['language'])){
 $id_show_company=1;
 include 'strings/strings_'.$_SESSION['language'].'.php';
 ?>
-<!--Initialize global variables-->
-<?php 
-$id_empresa_a_mostrar = 1; 
-?>
-<!--META, TITLE, LINKS-->
 <head>
-	<!--Required meta tags-->
 	<meta charset='UTF-8'>
 	<meta name='viewport' content='width=device-width, initial-scale=1, shrink-to-fit=no'>
 	<meta http-equiv='x-ua-compatible' content='ie=edge'>
-	<!-- Bootstrap CSS -->
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
-	<!-- Font Awesome CDN -->
 	<script src="https://use.fontawesome.com/aaefcbe50d.js"></script> 
-	<!-- Normlize -->
 	<link rel="stylesheet" type="text/css" href="normalize.css">  
-	<!-- Main CSS -->
 	<link rel="stylesheet" type="text/css" href="style.css">    
-	<!-- Tittle of the page -->
 	<title>
 		<?php
-		$cond = 'SELECT name from cms_company where id_company = $id_show_company';
+		$cond = 'SELECT name from cms_company where id_company = '.$id_show_company;
 		$result = mysqli_query($link, $cond);
 		if ($row = mysqli_fetch_array($result)){
 			echo $row['name'];
