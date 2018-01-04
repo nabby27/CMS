1f56c1c5e9a2657d3482f01f5bde619a2cafdae2
nabby27 <ivancordoba77@gmail.com>
2018-01-04 16:49:49 +0100

    Drop bootstrap and language select

--- head.php
+++ head.php
@@ -1,21 +1,14 @@
 <?php
-if (isset($_POST['language'])){ 
-	$_SESSION['language']=$_POST['language'];
-} 
-elseif (empty($_SESSION['language'])){
-	 $_SESSION['language']='es'; 
-}
 $id_show_company=1;
-include 'strings/strings_'.$_SESSION['language'].'.php';
+include 'strings/strings_es.php';
 ?>
 <head>
 	<meta charset='UTF-8'>
 	<meta name='viewport' content='width=device-width, initial-scale=1, shrink-to-fit=no'>
-	<meta http-equiv='x-ua-compatible' content='ie=edge'>
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
-	<script src="https://use.fontawesome.com/aaefcbe50d.js"></script> 
-	<link rel="stylesheet" type="text/css" href="normalize.css">  
-	<link rel="stylesheet" type="text/css" href="style.css">    
+	<meta http-equiv='x-ua-compatible' content='ie=edge'> 
+	<link rel = 'icon' type='image/ico' href='./img/favicon.ico'/>
+	<link href = './css/normalize.css' type='text/css' rel='stylesheet'>
+	<link href = './css/main_desingn.css' type='text/css' rel='stylesheet'>
 	<title>
 		<?php
 		$cond = 'SELECT name from cms_company where id_company = '.$id_show_company;
@@ -25,8 +18,4 @@ include 'strings/strings_'.$_SESSION['language'].'.php';
 		}
 		?>
 	</title>
-	<link rel = 'icon' type='image/ico' href='./img/favicon.ico'/>
-	<link href = './css/normalize.css' type='text/css' rel='stylesheet'>
-	<link href = './css/main_desingn.css' type='text/css' rel='stylesheet'>
-	<link href = './bootstrap/css/bootstrap.min.css' media='screen' rel='stylesheet'>
 </head>
--- scripts.php
+++ scripts.php
@@ -1,2 +1,3 @@
 <script src='https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
 <script src='js/bootstrap.min.js'></script>
+<script src="https://use.fontawesome.com/aaefcbe50d.js"></script> 
