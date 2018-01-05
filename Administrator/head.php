ae145c1e7a495cfe872fdbcff7d4a14d0f863655
nabby27 <ivancordoba77@gmail.com>
2018-01-05 11:45:31 +0100

    New structure css

--- Administrator/head.php
+++ Administrator/head.php
@@ -6,8 +6,8 @@ include 'functions.php';
 echo "<head>";
 	echo "<meta charset='UTF-8'>";
 	echo "<link rel='icon' type='image/ico' href='../cms_img/favicon.ico'/>";
-	echo "<link href='../user/css/normalize.css' type='text/css' rel='stylesheet'>";
-	echo "<link href='./css/main_design.css' type='text/css' rel='stylesheet'>";
+	echo "<link href='../css/normalize.css' type='text/css' rel='stylesheet'>";
+	echo "<link href='../css/administrator_style.css' type='text/css' rel='stylesheet'>";
 	echo "<title>";
 		$result = mysqli_query($link, "SELECT name from cms_company where id_company=$id_show_company");
 		while($row = mysqli_fetch_array($result)){
